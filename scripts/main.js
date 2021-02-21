870bb6b2f2623db6e8d083c5b1b8fb49eed293dd
LeonBaram <leonbaram@protonmail.com>
2021-02-21 16:43:19 -0500

    added functions implemented fetchRandomCard() implemented displayCard()

--- scripts/main.js
+++ scripts/main.js
@@ -8,19 +8,60 @@
 
 const mtgApp = {};
 
-mtgApp.url = new URL ('https://api.scryfall.com/cards/search');
+mtgApp.urls = {
+  random: 'https://api.scryfall.com/cards/random?q=',
+  named: ''
+};
+
+/**
+ * queries random mtg cards until it gets one of the specified type
+ * @param {string} type can be one of the following words:
+ * land, creature, artifact, enchantment, planeswalker, instant, sorcery
+ */
+mtgApp.fetchRandomCard = (type) => {
+
+  // start a loop
+  const MAX_LOOPS = 100;
+  let finished = false;
+  for (i = 0; !finished && i < MAX_LOOPS; i++) {
+    // fetch random card object
+    // set request parameter q=type
+    fetch(mtgApp.urls.random + type)
+    .then((response) => {
+      return response.json();
+    })
+    .then((card) => {
+
+      // if card is of desired type, exit loop
+      if (card.type_line.startsWith(type)) {
+        mtgApp.displayCard(card);
+        
+        finished = true;
+      }
+    });
+  }
+}
+
+mtgApp.displayCard = (card) => {
+
+  console.log(card);
+}
+
+mtgApp.fetchRandomCard("Land");
+
+/* mtgApp.url = new URL ('https://api.scryfall.com/cards/named');
 mtgApp.url.search = new URLSearchParams({
-  q: `island`
+    exact: `island`,
 });
 
 fetch(mtgApp.url)
   .then((response) => {
-    return response.json()
+    return response.json();
   })
     .then((result) => {
-      console.log(result.data);
+      console.log(result);
     })
-
+ */
 
 // 2. For the relevant cards (creature & planeswalker) grab additional information to be used in accompanying paragraph
 
