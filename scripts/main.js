6ef960198a1a486e244189b33d835ffca6d12dd8
andrewcampbell <awr.campbell@gmail.com>
2021-02-21 18:05:26 -0500

    refactored fetchRandomCard() to use await/async ES6 keywords, added cardTypes array

--- scripts/main.js
+++ scripts/main.js
@@ -10,7 +10,7 @@ const mtgApp = {};
 
 mtgApp.urls = {
   random: 'https://api.scryfall.com/cards/random?q=',
-  named: ''
+  named: 'https://api.scryfall.com/cards/named?fuzzy='
 };
 
 /**
@@ -18,7 +18,7 @@ mtgApp.urls = {
  * @param {string} type can be one of the following words:
  * land, creature, artifact, enchantment, planeswalker, instant, sorcery
  */
-mtgApp.fetchRandomCard = (type) => {
+mtgApp.fetchRandomCard = async (type) => {
 
   // start a loop
   const MAX_LOOPS = 100;
@@ -26,29 +26,41 @@ mtgApp.fetchRandomCard = (type) => {
   for (i = 0; !finished && i < MAX_LOOPS; i++) {
     // fetch random card object
     // set request parameter q=type
-    fetch(mtgApp.urls.random + type)
+    await fetch(mtgApp.urls.random)
     .then((response) => {
       return response.json();
     })
     .then((card) => {
-
       // if card is of desired type, exit loop
-      if (card.type_line.startsWith(type)) {
+      if (card.type_line.includes(type) && card.legalities.commander === "legal") {
         mtgApp.displayCard(card);
-        
+        console.log(card.type_line);
         finished = true;
-      }
+      };
     });
-  }
-}
+  };
+};
 
 mtgApp.displayCard = (card) => {
 
   console.log(card);
 }
 
-mtgApp.fetchRandomCard("Land");
+cardTypes = [
+  "Land",
+  "Creature",
+  "Artifact",
+  "Enchantment",
+  "Planeswalker",
+  "Instant",
+  "Sorcery"
+];
 
+for (let type of cardTypes) {
+  mtgApp.fetchRandomCard(type);
+}
+
+// boop
 /* mtgApp.url = new URL ('https://api.scryfall.com/cards/named');
 mtgApp.url.search = new URLSearchParams({
     exact: `island`,
