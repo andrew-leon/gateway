ed984e35923438907df2ba41ee046c6e7b9ce5c5
LeonBaram <leonbaram@protonmail.com>
2021-02-21 19:25:07 -0500

    minor code polishing removed references to q parameter for /random GET requests moved cardTypes array higher in the code (preserving order of data > funcs) improved comment consistency

--- scripts/main.js
+++ scripts/main.js
@@ -1,50 +1,6 @@
-// 1. Query the Scryfall API for one random card of each of the following types
-// 	- Land
-// 	- Creature
-// 	- Artifact
-// 	- Enchantment
-// 	- Planeswalker
-// 	- Instant or Sorcery
-
 const mtgApp = {};
 
-mtgApp.urls = {
-  random: 'https://api.scryfall.com/cards/random?q=',
-  // named: 'https://api.scryfall.com/cards/named?fuzzy='
-};
-
-/**
- * queries random mtg cards until it gets one of the specified type
- * @param {string} type can be one of the following words:
- * land, creature, artifact, enchantment, planeswalker, instant, sorcery
- */
-mtgApp.fetchRandomCard = async (type) => {
-
-  // start a loop
-  const MAX_LOOPS = 100;
-  let finished = false;
-  for (i = 0; !finished && i < MAX_LOOPS; i++) {
-    // fetch random card object
-    // set request parameter q=type
-    await fetch(mtgApp.urls.random)
-    .then((response) => {
-      return response.json();
-    })
-    .then((card) => {
-      // if card is of desired type, exit loop
-      if (card.type_line.includes(type) && card.legalities.commander === "legal") {
-        mtgApp.displayCard(card);
-        console.log(card.type_line);
-        finished = true;
-      };
-    });
-  };
-};
-
-mtgApp.displayCard = (card) => {
-  console.log(card);
-};
-
+// 1. Query the Scryfall API for one random card of each major type
 mtgApp.cardTypes = [
   "Land",
   "Creature",
@@ -55,24 +11,51 @@ mtgApp.cardTypes = [
   "Sorcery"
 ];
 
+mtgApp.urls = {
+  random: 'https://api.scryfall.com/cards/random',
+  // named: 'https://api.scryfall.com/cards/named?fuzzy='
+};
+
+/**
+ * queries random mtg cards until it gets one of the specified type
+ * @param {string} type can be one of the following words:
+ * land, creature, artifact, enchantment, planeswalker, instant, sorcery
+ */
+mtgApp.fetchRandomCard = async (type) => {
+
+  // attempt no more than MAX_LOOPS number of fetches
+  const MAX_LOOPS = 100;
+  let finished = false;
+  for (i = 0; !finished && i < MAX_LOOPS; i++) {
+
+    // fetch random card object
+    await fetch(mtgApp.urls.random)
+    .then((response) => {
+      return response.json();
+    })
+    .then((card) => {
+      // if this card is legal and of the specified type, display it and finish the loop
+      if (card.type_line.includes(type) && card.legalities.commander === "legal") {
+        mtgApp.displayCard(card);
+        finished = true;
+      };
+    });
+  };
+
+  // if we (somehow) failed to find the appropriate card in 100 attempts...
+  if (!finished) {
+    // add an image from a pre-selected local card cache?
+  }
+};
+
+mtgApp.displayCard = (card) => {
+  console.log(card);
+};
+
 for (let type of mtgApp.cardTypes) {
   mtgApp.fetchRandomCard(type);
 };
 
-/* mtgApp.url = new URL ('https://api.scryfall.com/cards/named');
-mtgApp.url.search = new URLSearchParams({
-    exact: `island`,
-});
+// 2. For the relevant cards (creature & planeswalker) grab additional information to be used in explanation text
 
-fetch(mtgApp.url)
-  .then((response) => {
-    return response.json();
-  })
-    .then((result) => {
-      console.log(result);
-    })
- */
-
-// 2. For the relevant cards (creature & planeswalker) grab additional information to be used in accompanying paragraph
-
-// 3. Display cards on page using objects url property as img src and card name and type as alt text
+// 3. Display cards on page using object url as img src, and card name/type as alt text
